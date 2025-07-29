40b923456236860d45474eb8f8d576626b249fbf
hein98 <horux1998@gmail.com>
2025-07-29 21:09:19 +0700

    a new HTML template & static files update

--- travel/travel_app/views.py
+++ travel/travel_app/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def home(request):
-    return render(request,'home.html',{'name':'Django World'})
+def index(request):
+    return render(request,'index.html')
 
